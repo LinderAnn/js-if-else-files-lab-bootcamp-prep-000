05e7277e68a58b058d0012e566a8b931761d9265
Ann Linder <linder.ann@gmail.com>
2017-10-24 19:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var name = "Susan"
+var name = "Joes"
 var height = "74"
 
 if (name === "Susan") {
-  console.log("The name is correct")
+    console.log("The name is correct")
 } else {
-  console.log("Expected the name to be Susan")
+    console.log("Expected the name to be Susan")
 }
-
 
 // Don't worry about this
 module.exports = { name, height
